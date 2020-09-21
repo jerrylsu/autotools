02c4859adf467e8347b07f683b1a6955bdd9774c
Jerry <sa517301@mail.ustc.edu.cn>
2020-09-21 08:04:50 +0000

    remove .pyc

--- pyutils/blog.py
+++ pyutils/blog.py
@@ -10,11 +10,11 @@ def rename_articles(file_pattern):
     files = glob.glob(file_pattern)
     for file_ in files:
         print(file_)
-        raise "Test..."
         date = ''
         with open(file_, 'r', encoding='utf-8') as fin:
             lines = fin.readlines()
         for i, line in enumerate(lines):
             if line.startswith('Date: '):
                 date = line.split()[1]
+                print(date)
                 break
