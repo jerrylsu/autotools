b616ba8cace0f664709862c90f39582794442733
“jerry” <sa517301@mail.ustc.edu.cn>
2022-11-21 12:33:19 +0800

    Add jupyter mudule.

--- autotools/__init__.py
+++ autotools/__init__.py
@@ -1,8 +1,7 @@
 """This is a Python pacakage that contains automation tools.
 """
-from git import Git
-from pelican import Pelican
-from jupyter import JupyterLab, NoteBook
-
-
 __version__ = '0.1.0'
+from .git import Git
+from .pelican import Pelican
+from .jupyter import JupyterLab, NoteBook
+
