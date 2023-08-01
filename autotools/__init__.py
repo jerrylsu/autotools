487beb25c1bd750a138c50ca7a476ae1f2977950
“jerry” <sa517301@mail.ustc.edu.cn>
2023-08-01 20:01:57 +0800

    Add elasticsearch module.

--- autotools/__init__.py
+++ autotools/__init__.py
@@ -1,7 +1,8 @@
 """This is a Python pacakage that contains automation tools.
 """
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 from .git import Git
 from .jupyter import JupyterLab, NoteBook
 from .pdf import *
+from .es import *
 
---
+++ autotools/es.py
@@ -0,0 +1,168 @@
+"""ElasticSearch tools.
+"""
+import json
+import elasticsearch
+from elasticsearch.helpers import streaming_bulk
+from tqdm import tqdm
+from typing import List, Dict, Optional, NamedTuple, Iterator
+from loguru import logger
+
+
+class BatchedSearchResults(NamedTuple):
+    total_scores: List[List[float]] = None
+    total_indices: List[List[int]] = None
+
+
+class ElasticSearchIndex(object):
+    """
+    Sparse index using Elasticsearch. It is used to index text and run queries based on BM25 similarity.
+    An Elasticsearch server needs to be accessible, and a python client is declared with
+    ```
+    es_client = Elasticsearch([{'host': 'localhost', 'port': '9200'}])
+    ```
+    """
+    def __init__(self, host: Optional[str] = "localhost", port: Optional[int] = 9200):
+        self.es_client = elasticsearch.Elasticsearch(hosts=[{"host": host, "port": port, "scheme": "http"}],
+                                                     # http_auth=(es_user, es_passwd),
+                                                     max_retries=3,
+                                                     request_timeout=120,
+                                                     retry_on_timeout=True)
+        logger.info("ElasticSearch connected {}:{} successfully.".format(host, str(port)))
+
+    def create_index(self, index_name: str, index_file: str) -> str:
+        """Create elasticsearch index.
+        https://www.elastic.co/guide/en/elasticsearch/reference/current/dense-vector.html
+
+        Args:
+            index_name (`str`): The index name.
+            index_file (`Dict`): The index mapping file.
+        Output:
+            status (`str`): The status, success "OK", fail "ERROR".
+        """
+        status = "OK"
+        if self.es_client.indices.exists(index=index_name):
+            logger.info(f"ES index [{index_name}] already exists.")
+            return status
+
+        with open(index_file, "r") as index_file:
+            source = json.load(index_file)
+
+        try:
+            ret = self.es_client.indices.create(index=index_name, body=source)
+            if not ret["acknowledged"]:
+                logger.error(f"ES index create failed: {e}")
+                status = "ERROR"
+            logger.info(f"Create ES index: {index_name}.")
+        except Exception as e:
+            logger.error(f"ES index create failed: {e}")
+            status = "ERROR"
+        return status
+
+    def delete_index(self, index_name: str):
+        """Delete elasticsearch index.
+
+        Args:
+            index_name (`str`): The index name.
+        Output:
+            status (`str`): The status, success "OK", fail "ERROR".
+        """ 
+        status = "OK"
+        if not self.es_client.indices.exists(index=index_name):
+            logger.info(f"Not exists ES index: {index_name}.")
+            return status
+
+        try:
+            ret = self.es_client.indices.delete(index=index_name)
+            if not ret["acknowledged"]:
+                logger.error(f"ES index delete failed: {e}")
+                status = "ERROR"
+            logger.info(f"Delete ES index: {index_name}.")
+        except Exception as e:
+            logger.error(f"ES index delete failed: {e}")
+            status = "ERROR"
+        return status
+
+    def add_documents(self, index_name: str, docs_generator: Iterator) -> str:
+        """Add documents to the elasticsearch index.
+
+        Args:
+            index_name (`str`): The index name.
+            docs_generator (`Iterator`): The documents generator.
+        Output:
+            status (`str`): The status, success "OK", fail "ERROR".
+        """
+        # 1. Create elasticsearch index.
+        if not self.es_client.indices.exists(index=index_name):
+            status = self.create_index(index_name=index_name)
+            if status == "ERROR":
+                return status
+
+        # 2. Add documents to elasticsearch.
+        successes = 0
+        progress = tqdm(unit="docs")
+        for ok, action in streaming_bulk(client=self.es_client, index=index_name, actions=docs_generator):
+            progress.update(1)
+            successes += ok
+
+        logger.info(f"Add documents to index [{index_name}]: {successes}.")
+        return status
+
+    def search(self, index_name: str, doc_vector_name: str, query_vector: List[float]) -> Dict:
+        """Find the nearest documents indices to the query.
+
+        Args:
+            index_name (`str`): The index name.
+            doc_vector_name (`str`): The document vector key name.
+            query_vector (`List[float]`): The query vector.
+        Output:
+            response (`Dict`): The response of elasticsearch.
+        """
+        script_query = {
+            "query": {
+                "script_score": {
+                    "query": {
+                        "match_all": {}
+                    },
+                    "script": {
+                        "source": f"doc[{doc_vector_name}].size() == 0 ? 0 : cosineSimilarity(params.query_vector, {doc_vector_name}) + 1.0",
+                        "params": {
+                            "query_vector": query_vector
+                        }
+                    }
+                }
+            }
+        }
+
+        try:
+            search_start = time.time()
+            response = self.es_client.search(index=index_name, body=script_query)
+            search_time = time.time() - search_start
+            logger.info(f"Search time: {search_time}")
+        except Exception as e:
+            logger.error(f"Search the nearest documents indices {index_name} error: {e}.")
+            return {}
+        return response
+
+    def search_batch(self, queries: List[str], size: int = 5, max_workers: int = 10) -> BatchedSearchResults:
+        """Find the nearest documents indices to the batch query.
+
+        Args:
+            queries (`str`): The query as a string.
+            size (`int`): The number of documents to retrieve.
+            max_workers (`int`): The number of thread.
+        Output:
+            scores (`List[List[float]`): The retrieval scores of the retrieved documents.
+            indices (`List[List[int]]`): The indices of the retrieved documents.
+        """
+        import concurrent.futures
+
+        total_scores, total_indices = [None] * len(queries), [None] * len(queries)
+        with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+            future_to_index = {executor.submit(self.search, query, size): i for i, query in enumerate(queries)}
+            for future in concurrent.futures.as_completed(future_to_index):
+                index = future_to_index[future]
+                results: SearchResults = future.result()
+                total_scores[index] = results.scores
+                total_indices[index] = results.indices
+        return BatchedSearchResults(total_scores=total_scores, total_indices=total_indices)
+
